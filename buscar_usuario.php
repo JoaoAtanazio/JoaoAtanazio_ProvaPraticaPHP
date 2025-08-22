b81351c115cf69d4bba856b91d6c6efeff8a2f71
João Vitor Atanazio <joao_atanazio@estudante.sesisenai.org.br>
2025-08-22 14:15:18 -0300

    buscar usuario
    tela para buscar os dados do usuário escolhido, possui estilização, menu dropdown para navegabilidade, botão que leva para a tela de alterar e excluir, e cadastra no banco de dados

---
+++ buscar_usuario.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+require_once 'conexao.php';
+require_once 'permissoes.php';
+require_once 'dropdown.php';
+
+
+// VERIFICA SE O USUARIO TEM PERMISSAO DE adm OU secretaria
+if($_SESSION['perfil']!=1 && $_SESSION['perfil']!=2){
+    echo "<script> alert ('Acesso negado!');window.location.href='principal.php';</script>";
+    exit();
+}
+
+$usuario = []; // INICIALIZA A VARIAVEL PARA EVITAR ERROS
+
+// SE O FORMULARIO FOR ENVIADO, BUSCA O USUARIO PELO ID OU NOME
+if($_SERVER["REQUEST_METHOD"]== "POST" && !empty($_POST['busca'])){
+    $busca = trim($_POST['busca']);
+
+    // VERIFICA SE A BUSCA É UM numero OU UM nome
+    if(is_numeric($busca)){
+        $sql="SELECT * FROM usuario WHERE id_usuario = :busca ORDER BY nome ASC";
+        $stmt=$pdo->prepare($sql);
+        $stmt->bindParam(':busca', $busca, PDO::PARAM_INT);
+    }else{
+        $sql="SELECT * FROM usuario WHERE nome LIKE :busca_nome ORDER BY nome ASC";
+        $stmt=$pdo->prepare($sql);
+        $stmt->bindValue(':busca_nome', "$busca%", PDO::PARAM_STR);
+    }
+} else{
+    $sql="SELECT * FROM usuario ORDER BY nome ASC";
+    $stmt=$pdo->prepare($sql);
+}
+$stmt->execute();
+$usuarios = $stmt->fetchALL(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Buscar Usuário</title>
+</head>
+<body>
+    <h2> Lista de usuários </h2>
+
+    <form action="buscar_usuario.php" Method="POST">
+        <label for="busca"> Digite o ID ou NOME(opcional): </label>
+        <input type="text" id="busca" name="busca">
+        <button type="submit">Pesquisar</button>
+    </form>
+    <?php if(!empty($usuarios)):?>
+        <table class="table">
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Perfil</th>
+                <th>Ações</th>
+            </tr>
+        <?php foreach($usuarios as $usuario): ?>
+            <tr>
+                <td><?=htmlspecialchars($usuario['id_usuario'])?></td>
+                <td><?=htmlspecialchars($usuario['nome'])?></td>
+                <td><?=htmlspecialchars($usuario['email'])?></td>
+                <td><?=htmlspecialchars($usuario['id_perfil'])?></td>
+                <td>
+                    <a href="alterar_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>">Alterar</a>  
+
+                    <a href="excluir_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>"onclick="return confirm('Tem certeza que você quer excluir?')">Excluir</a>                 
+                </td>
+            </tr>
+        <?php endforeach;?>    
+        </table>
+    <?php else:?>
+        <p> Nenhum usuario encontrado.</p>
+    <?php endif;?>
+    
+    <a href="principal.php" class="btn-voltar"> VOLTAR</a>
+</body>
+</html>
